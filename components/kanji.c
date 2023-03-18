053885d16976fc14148bd49adf687e1a80f74494
Drew Herron <herron@mailbox.org>
2023-03-17 17:44:53 -0700

    Kanji comments for reference

--- components/kanji.c
+++ components/kanji.c
@@ -11,3 +11,34 @@ kanji(const char *unused) {
 	int wd=(y+y/4-y/100+y/400+map[m-1]+tm.tm_mday)%7;
 	return kanji[wd-1];
 }
+
+/*
+Sunday		日曜日	nichiyoubi
+Monday		月曜日	getsuyoubi
+Tuesday		火曜日	kayoubi
+Wednesday	水曜日	suiyoubi
+Thursday	木曜日	mokuyoubi
+Friday		金曜日	kin'youbi
+Saturday	土曜日	doyoubi
+
+nichi 		日 			Day
+hi (taiyou) 日（太陽）	Sun
+
+getsu 		月 			Month
+tsuki 		月          Moon
+
+hi 			火 			Fire
+kasei 		火星 		Mars
+
+mizu 		水 			Water
+suisei 		水星 		Mercury
+
+ki 			木 			Tree
+mokusei		木星 		Jupiter
+
+kin 		金 			Gold
+kinsei 		金星 		Venus
+
+tsuchi 		土 			Soil
+dosei 		土星 		Saturn
+*/
